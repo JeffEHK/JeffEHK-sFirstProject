bccec8934264998dc3c914468c8ba160952b7bcd
JeffEHK <155628477+JeffEHK@users.noreply.github.com>
2024-01-04 18:23:49 +0800

    Update Demo.java

--- FirstProjectInGitHubOfMine/src/Demo.java
+++ FirstProjectInGitHubOfMine/src/Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hello GitHub...\nÄãºÃGitHub...");
+        System.out.println("hello GitHub...\nÃ„Ã£ÂºÃƒGitHub...");
     }
 }
